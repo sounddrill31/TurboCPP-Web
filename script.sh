e75aefc61422fd29618071865c3c6b62a0ee1f36
SoundDrill 31 <sounddrill31@gmail.com>
2024-09-07 11:36:55 +0000

    Rely on scripts to start it

--- script.sh
+++ script.sh
@@ -7,7 +7,4 @@ sudo apt install mkisofs -y
 rm *.iso *.img || true
 
 
-#mkisofs -o output.iso -V CDRV prebuilts/turbocpp/
-genisoimage -o turbocpp.img -R -J -V "TurboCPP" /workspace/TurboCPP-Web/prebuilts/turbocpp
-mtools -i turbocpp.img -c "mformat a: -F -C -B 2048 -S 512 -h 16 -t 63"
-mtools -i turbocpp.img -c "mcopy -s $(realpath .)/prebuilts/turbocpp/* a:"
+mkisofs -o output.iso -V CDRV prebuilts/turbocpp/
